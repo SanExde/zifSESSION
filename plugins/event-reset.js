4ca61bc1fc72a20ad3c73880567b64a52a9b766c
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-31 23:57:02 +0700

    Delete plugins directory

--- plugins/event-reset.js
+++
@@ -1,7 +0,0 @@
-let handler = async(m, { conn }) => {
-    let users = Object.entries(db.data.users).map(v => v[0])
-    for (id of users) db.data.users[id].reward = 0
-}
-handler.owner = true
-handler.command = /^event-reset$/i
-module.exports = handler
